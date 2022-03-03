99fd61ad5e808e5d853b086e82e74206480a7d10
sachin kumar <sachin.sachin.kumar17@gmail.com>
2022-03-03 16:43:41 +0530

    First Commit

---
+++ src/calculator/Add.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Add {
+	
+	public static void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
